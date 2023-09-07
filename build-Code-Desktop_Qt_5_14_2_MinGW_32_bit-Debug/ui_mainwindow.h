0884e8c7f4a75414fcdcfa5768b945eb4079eddd
wwnnt <2773104788@qq.com>
2023-09-07 22:42:45 +0800

    9月7号第一次更新

---
+++ build-Code-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,95 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *action;
+    QAction *action_2;
+    QAction *action_3;
+    QAction *action_4;
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *menu;
+    QMenu *menu_2;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(899, 706);
+        action = new QAction(MainWindow);
+        action->setObjectName(QString::fromUtf8("action"));
+        action_2 = new QAction(MainWindow);
+        action_2->setObjectName(QString::fromUtf8("action_2"));
+        action_3 = new QAction(MainWindow);
+        action_3->setObjectName(QString::fromUtf8("action_3"));
+        action_4 = new QAction(MainWindow);
+        action_4->setObjectName(QString::fromUtf8("action_4"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 899, 23));
+        menu = new QMenu(menubar);
+        menu->setObjectName(QString::fromUtf8("menu"));
+        menu_2 = new QMenu(menubar);
+        menu_2->setObjectName(QString::fromUtf8("menu_2"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menu->menuAction());
+        menubar->addAction(menu_2->menuAction());
+        menu->addAction(action);
+        menu->addAction(action_2);
+        menu_2->addAction(action_3);
+        menu_2->addAction(action_4);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        action->setText(QCoreApplication::translate("MainWindow", "\346\267\273\345\212\240", nullptr));
+        action_2->setText(QCoreApplication::translate("MainWindow", "\344\277\256\346\224\271", nullptr));
+        action_3->setText(QCoreApplication::translate("MainWindow", "\346\267\273\345\212\240", nullptr));
+        action_4->setText(QCoreApplication::translate("MainWindow", "\344\277\256\346\224\271", nullptr));
+        menu->setTitle(QCoreApplication::translate("MainWindow", "\350\247\222\350\211\262", nullptr));
+        menu_2->setTitle(QCoreApplication::translate("MainWindow", "\350\243\205\345\244\207", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
